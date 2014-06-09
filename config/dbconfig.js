81c104ec7297db41395ef4470e9393dd6b730061
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2014-06-09 03:18:01 -0300

    removing node_modules/ folder from git

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -20,7 +20,8 @@ var dbConfig =  {
 			url: "mongodb://localhost/euquerominhabiblioteca_test"
 		},
 		development: {
-			url: "mongodb://admin:@localhost/EcoDB"
+			//url: "mongodb://localhost/euquerominhabiblioteca_development", //"mongodb://admin:@localhost/EcoDB"
+			url: "mongodb://heroku:r3ldnJP10iZ_t2o4__1wtRLxPrLuab6tFusPrRggd8i605FuMqrzNMJppOpRJQeSYvx9uogalQmbw2meX1P_pw@oceanic.mongohq.com:10078/app25614466"
 		},
 		production: {
 			url: process.env.MONGOHQ_URL
@@ -30,11 +31,11 @@ var dbConfig =  {
 		test: {
 			url: {
 				host: 'localhost',
-      			port: 6379,
-      			db: 2,
-      			prefix: '_session',
-      			pass: 'test'
-      		}
+    			port: 6379,
+    			db: 2,
+    			prefix: '_session',
+    			pass: 'test'
+    		}
      	},
 		development: {
       		url: parseRedisURL('redis://admin:5V8Gnx3D@localhost:6379')
